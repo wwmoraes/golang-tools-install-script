9db59fa59e763a4e9861065edc706c15c8e1415c
Jorge Canha <jcanha@gmail.com>
2017-07-07 23:06:15 +0100

    WIP

--- tests/main.sh
+++ tests/main.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 set -e
 
+../goinstall.sh --64
+
+source ~/.bashrc
+
+mkdir -p $GOPATH/src/hello
+pushd $GOPATH/src/hello
+cat >hello.go <<EOF
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("hello, world\n")
+}
+EOF
+go build
+hello
+popd
